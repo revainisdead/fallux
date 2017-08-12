5b4e86e0633c4a22567352ba5f1c591972b7a6a6
Christian Hall <hallbasics@yahoo.com>
2017-08-11 20:10:36 -0700

    Compile lib and add linker.
     - Also fill out main for first scene creation.

--- Makefile
+++ Makefile
@@ -2,14 +2,21 @@
 
 EXE_NAME= fallux.exe
 WORKING_DIR= $(shell pwd)
+#RELATIVE_WORKING_DIR= ${CUR_DIR}
 IRR_DIR= $(WORKING_DIR)/lib/irrlicht-1.8.4
-IRR_LIB= $(IRR_DIR)/lib/
-IRR_INCLUDE= $(IRR_DIR)/include/
+IRR_LIB= $(IRR_DIR)/lib/Linux
+IRR_INCLUDE= $(IRR_DIR)/include
+
+# Replace all backslashes with escaped backslashes.
+#BUILD_WHITELIST= $(shell echo $(shell pwd)/lib/ | sed -e "s:/:\\\/:g")
+
+# git ls-files prints relative paths, use those here for exclusion.
+BUILD_WHITELIST= lib/
 
 all: build run
 
 build:
-	g++ $$(git ls-files -- "*.cpp") -I $(IRR_INCLUDE) -L $(IRR_LIB) -Wall -o $(EXE_NAME)
+	g++ $$(git ls-files -- "*.cpp" | grep -v $(BUILD_WHITELIST)) -I$(IRR_INCLUDE) -L$(IRR_LIB) -lIrrlicht -lGLU -lGL -lX11 -lXxf86vm -Wall -o $(EXE_NAME)
 
 run:
 	@./$(EXE_NAME)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 #include <irrlicht.h>
 
-
+using namespace irr;
 int main() {
     std::cout << "test out" << std::endl;
 
+    IrrlichtDevice *device = createDevice(video::EDT_SOFTWARE, core::dimension2d<u32>(640, 480), 16, false, false, false, 0);
+
+    if (!device)
+        return 1;
+
+    device->setWindowCaption(L"Irrlicht Engine");
+
+    video::IVideoDriver *driver = device->getVideoDriver();
+    scene::ISceneManager *smgr = device->getSceneManager();
+    gui::IGUIEnvironment *guienv = device->getGUIEnvironment();
+
+    guienv->addStaticText(L"Hello World!", core::rect<s32>(10, 10, 260, 22), true);
+
+    // TODO: Fix this absolute path later.
+    scene::IAnimatedMesh *mesh = smgr->getMesh("/home/christian/bin/fallux/res/meshes/sydney.md2");
+    if (!mesh) {
+        device->drop();
+        return 1;
+    }
+    scene::IAnimatedMeshSceneNode *node = smgr->addAnimatedMeshSceneNode(mesh);
+
+    if (node) {
+        node->setMaterialFlag(video::EMF_LIGHTING, false);
+        node->setMD2Animation(scene::EMAT_STAND);
+        node->setMaterialTexture(0, driver->getTexture("/home/christian/bin/fallux/res/textures/sydney.bmp"));
+    }
+
+    smgr->addCameraSceneNode(0, core::vector3df(0, 30, -40), core::vector3df(0, 5, 0));
+
+    while (device->run()) {
+        driver->beginScene(true, true, video::SColor(255, 100, 101, 140));
+
+        smgr->drawAll();
+        guienv->drawAll();
+
+        driver->endScene();
+    }
+
+    device->drop();
+    return 0;
 }
